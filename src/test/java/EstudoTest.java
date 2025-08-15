e3a1fa2a858d3a6d7e377fd24c8b057d992adcb7
Menin <meninvalterpedro@gmail.com>
2025-08-15 07:47:30 +0100

    pgm corrigido

--- src/test/java/EstudoTest.java
+++ src/test/java/EstudoTest.java
@@ -1,2 +1,85 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
 public class EstudoTest {
+
+    //ATRIBUTOS
+
+    private static WebDriver driver;
+
+    //FUNÇÕES, MÉTODOS
+
+    //ANTES DO TESTE
+
+    @BeforeAll
+
+    public static void setUp() {
+
+        //WebDriverManager.chromedriver().setup();
+
+        System.setProperty("webdriver.edge.driver", "src/test/driver/msedgedriver.exe");
+
+        driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+    }
+
+
+
+    @BeforeEach
+
+    public void inicioTesteUnico () {
+
+        driver.get("https://front.serverest.dev/login");
+
+    }
+
+
+
+    //DEPOIS DO TESTE
+
+    @AfterAll
+
+    public static void tearDown() {
+
+        driver.quit();
+
+    }
+
+
+
+    //TESTE
+
+    @Test
+
+    public void test02() {
+
+        driver.findElement(By.linkText("Cadastre-se")).click();
+
+        driver.findElement(By.id("nome")).click();
+
+        driver.findElement(By.id("nome")).sendKeys("menin");
+
+        driver.findElement(By.id("email")).click();
+
+        driver.findElement(By.id("email")).sendKeys("menin2@gmail.com");
+
+        driver.findElement(By.id("password")).click();
+
+        driver.findElement(By.id("password")).sendKeys("asasas");
+
+        driver.findElement(By.id("administrador")).click();
+
+        driver.findElement(By.cssSelector(".btn-primary")).click();
+
+    }
+
 }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,8 +1,8 @@
 package org.example;
 
-import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
@@ -23,7 +23,7 @@ public class AppTest
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
+    public static TestSuite suite()
     {
         return new TestSuite( AppTest.class );
     }
