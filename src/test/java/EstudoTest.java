7d06bebbdf2861043551752aae9b4e9ba4462c83
Menin <meninvalterpedro@gmail.com>
2025-08-16 09:14:59 +0100

    pgm corrigido

--- src/test/java/EstudoTest.java
+++ src/test/java/EstudoTest.java
@@ -1,12 +1,10 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+//import org.openqa.selenium.edge.EdgeDriver;
+import java.time.Duration;
 
 public class EstudoTest {
 
@@ -17,70 +15,46 @@ public class EstudoTest {
     //FUNÇÕES, MÉTODOS
 
     //ANTES DO TESTE
-
     @BeforeAll
 
     public static void setUp() {
-
-        //WebDriverManager.chromedriver().setup();
-
-        System.setProperty("webdriver.edge.driver", "src/test/driver/msedgedriver.exe");
-
-        //driver = new ChromeDriver();
-        driver = new EdgeDriver();
-
+        WebDriverManager.chromedriver().setup();
+        //System.setProperty("webdriver.edge.driver", "src/test/driver/msedgedriver.exe");
+        driver = new ChromeDriver();
+        //driver = new EdgeDriver();
         driver.manage().window().maximize();
-
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
     }
-
 
 
     @BeforeEach
-
-    public void inicioTesteUnico () {
-
+    public void padraoCarregamento () {
         driver.get("https://front.serverest.dev/login");
-
     }
-
 
 
     //DEPOIS DO TESTE
-
     @AfterAll
-
     public static void tearDown() {
-
+        System.out.println("Teste Finalizado!!!");
         driver.quit();
-
     }
-
 
 
     //TESTE
-
     @Test
-
-    public void test02() {
-
+    @DisplayName("Teste realizado no caminho feliz com sucesso.")
+    public void testCadastroSimplesComDadosValidos() {
         driver.findElement(By.linkText("Cadastre-se")).click();
-
         driver.findElement(By.id("nome")).click();
-
-        driver.findElement(By.id("nome")).sendKeys("menin");
-
+        driver.findElement(By.id("nome")).sendKeys("valter311");
         driver.findElement(By.id("email")).click();
-
-        driver.findElement(By.id("email")).sendKeys("menin2@gmail.com");
-
+        driver.findElement(By.id("email")).sendKeys("menninni211@gmail.com");
         driver.findElement(By.id("password")).click();
-
-        driver.findElement(By.id("password")).sendKeys("asasas");
-
+        driver.findElement(By.id("password")).sendKeys("a11111111");
         driver.findElement(By.id("administrador")).click();
-
         driver.findElement(By.cssSelector(".btn-primary")).click();
+        driver.findElement(By.linkText("Listar")).click();
 
     }
-
 }
